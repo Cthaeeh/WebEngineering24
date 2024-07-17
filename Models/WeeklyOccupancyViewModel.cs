3d0f6088c541e634bdea1657aebe49c57223c13a
sd1314132 <167768217+sd1314132@users.noreply.github.com>
2024-07-17 11:26:27 +0200

    class_added

---
+++ Models/WeeklyOccupancyViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace MyWebDbApp.Models
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class WeeklyOccupancyViewModel
+    {
+        public DateTime WeekStartDate { get; set; }
+        public DateTime WeekEndDate { get; set; }
+        public List<Occupancy> Occupancies { get; set; }
+    }
+}
