83630134b154b8cb31559c840c05d7365538e718
sd1314132 <167768217+sd1314132@users.noreply.github.com>
2024-07-17 10:49:33 +0200

    working_on_it

--- Controllers/OccupanciesController.cs
+++ Controllers/OccupanciesController.cs
@@ -1,36 +1,36 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
-using MyWebDbApp.Models;
-using MyWebDbApp.Data;
-using Microsoft.AspNetCore.Authorization;
-using System.Security.AccessControl;
+using Deskbuddy.Data;
+using Deskbuddy.Models;
 using Microsoft.AspNetCore.Identity;
-using MyWebDbApp.Areas.Identity.Data;
+using Deskbuddy.Areas.Identity.Data;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
 
-namespace MyWebDbApp.Controllers
+namespace Deskbuddy.Controllers
 {
-    [Authorize(Roles = "Administrator, Office, Chief, Employee")]
+    [Authorize(Roles ="Sekretariat, Abteilungsleiter, Mitarbeiter")]
     public class OccupanciesController : Controller
     {
-        private readonly AppDbContext _context;
-        private readonly UserManager<AppUser> _userManager;
+        private readonly DeskbuddyContext _context;
+        private readonly UserManager<DeskbuddyUser> _userManager;
 
-        public OccupanciesController(AppDbContext context, UserManager<AppUser> userManager)
+        public OccupanciesController(DeskbuddyContext context, UserManager<DeskbuddyUser> userManager)
         {
             _context = context;
             _userManager = userManager;
         }
 
-        // GET: Occupancies 
+        // GET: Occupancies
         public async Task<IActionResult> Index()
         {
-            var occupancies = _context.Occupancies.Include(o => o.Employee).Include(o => o.Workspace);
-            return View(await occupancies.ToListAsync());
+            var deskbuddyContext = _context.Occupancies.Include(o => o.Employee).Include(o => o.Room);
+            return View(await deskbuddyContext.ToListAsync());
         }
 
         // GET: Occupancies/Details/5
@@ -43,7 +43,7 @@ namespace MyWebDbApp.Controllers
 
             var occupancy = await _context.Occupancies
                 .Include(o => o.Employee)
-                .Include(o => o.Workspace)
+                .Include(o => o.Room)
                 .FirstOrDefaultAsync(m => m.Id == id);
             if (occupancy == null)
             {
@@ -54,17 +54,20 @@ namespace MyWebDbApp.Controllers
         }
 
         // GET: Occupancies/Create
+        [Authorize(Roles = "Sekretariat")]
         public IActionResult Create()
         {
             ViewData["EmployeeId"] = new SelectList(_context.Employees, "Id", "Name");
-            ViewData["WorkplaceId"] = new SelectList(_context.Workspaces, "Id", "RoomId");
+            ViewData["RoomId"] = new SelectList(_context.Rooms, "Id", "Name");
             return View();
         }
 
         // POST: Occupancies/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,WorkplaceId,EmployeeId,Date")] Occupancy occupancy)
+        public async Task<IActionResult> Create([Bind("Id,RoomId,EmployeeId,Date,Features")] Occupancy occupancy)
         {
             if (ModelState.IsValid)
             {
@@ -72,11 +75,11 @@ namespace MyWebDbApp.Controllers
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
-            ViewData["EmployeeId"] = new SelectList(_context.Employees, "Id", "Name");
-            ViewData["WorkplaceId"] = new SelectList(_context.Workspaces, "Id", "RoomId");
+            ViewData["EmployeeId"] = new SelectList(_context.Employees, "Id", "Name", occupancy.EmployeeId);
+            ViewData["RoomId"] = new SelectList(_context.Rooms, "Id", "Name", occupancy.RoomId);
             return View(occupancy);
         }
-
+        [Authorize(Roles ="Sekretariat, Mitarbeiter")]
         // GET: Occupancies/Edit/5
         public async Task<IActionResult> Edit(int? id)
         {
@@ -91,27 +94,53 @@ namespace MyWebDbApp.Controllers
                 return NotFound();
             }
 
-            // Check if the current user has the role "Mitarbeiter" and if the occupancy belongs to them
             var currentUser = await _userManager.GetUserAsync(User);
-            if (!User.IsInRole("Sekretariat") && !User.IsInRole("Abteilungsleiter") && occupancy.EmployeeId != currentUser.Id)
+            var existingOccupancy = await _context.Occupancies.FindAsync(id);
+
+            if (existingOccupancy == null)
             {
-                return Forbid(); // Or handle unauthorized access
+                return NotFound();
+            }
+
+            if (User.IsInRole("Mitarbeiter"))
+            {
+
+                // Check if the current user can edit the specified occupancy
+                if (existingOccupancy.UserId != currentUser.Id)
+                {
+                    // If the current user is not allowed to edit this occupancy, return an unauthorized view or handle accordingly
+                    return Forbid();
+                }
+
+                // If the current user is allowed to edit, proceed to show the edit view
+                return View(existingOccupancy);
             }
 
             ViewData["EmployeeId"] = new SelectList(_context.Employees, "Id", "Name", occupancy.EmployeeId);
-            ViewData["WorkplaceId"] = new SelectList(_context.Workspaces, "Id", "RoomId", occupancy.WorkspaceId);
+            ViewData["RoomId"] = new SelectList(_context.Rooms, "Id", "Name", occupancy.RoomId);
             return View(occupancy);
         }
 
         // POST: Occupancies/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,WorkplaceId,EmployeeId,Date")] Occupancy occupancy)
+        public async Task<IActionResult> Edit(int id, [Bind("Id,RoomId,EmployeeId,Date,Features")] Occupancy occupancy)
         {
             if (id != occupancy.Id)
             {
                 return NotFound();
             }
+
+            var currentUser = await _userManager.GetUserAsync(User);
+            var existingOccupancy = await _context.Occupancies.FindAsync(id);
+
+            if (existingOccupancy == null)
+            {
+                return NotFound();
+            }
+
 
             if (ModelState.IsValid)
             {
@@ -134,11 +163,12 @@ namespace MyWebDbApp.Controllers
                 return RedirectToAction(nameof(Index));
             }
             ViewData["EmployeeId"] = new SelectList(_context.Employees, "Id", "Name", occupancy.EmployeeId);
-            ViewData["WorkplaceId"] = new SelectList(_context.Workspaces, "Id", "RoomId", occupancy.WorkspaceId);
+            ViewData["RoomId"] = new SelectList(_context.Rooms, "Id", "Name", occupancy.RoomId);
             return View(occupancy);
         }
 
         // GET: Occupancies/Delete/5
+        [Authorize(Roles = "Sektretariat")]
         public async Task<IActionResult> Delete(int? id)
         {
             if (id == null)
@@ -148,7 +178,7 @@ namespace MyWebDbApp.Controllers
 
             var occupancy = await _context.Occupancies
                 .Include(o => o.Employee)
-                .Include(o => o.Workspace)
+                .Include(o => o.Room)
                 .FirstOrDefaultAsync(m => m.Id == id);
             if (occupancy == null)
             {
@@ -176,6 +206,30 @@ namespace MyWebDbApp.Controllers
         private bool OccupancyExists(int id)
         {
             return _context.Occupancies.Any(e => e.Id == id);
+        }
+
+        public IActionResult WeeklyOccupancyPreview()
+        {
+            // Calculate the start of the current week
+            DateTime startOfWeek = DateTime.Today.Date.AddDays(-(int)DateTime.Today.DayOfWeek);
+            // Calculate the end of the current week
+            DateTime endOfWeek = startOfWeek.AddDays(6);
+
+            var occupancies = _context.Occupancies
+                                    .Include(o => o.Employee)
+                                    .Include(o => o.Room)
+                                    .Where(o => o.Date >= startOfWeek && o.Date <= endOfWeek)
+                                    .OrderBy(o => o.Date)
+                                    .ToList();
+
+            var viewModel = new WeeklyOccupancyViewModel
+            {
+                WeekStartDate = startOfWeek,
+                WeekEndDate = endOfWeek,
+                Occupancies = occupancies
+            };
+
+            return View("WeeklyOccupancyViewModel", viewModel);
         }
     }
 }
